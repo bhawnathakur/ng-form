21e6c68d487f4465900b18484dda8c352eb1b536
bhawnathakur <bhawnathakur2@gmail.com>
2016-12-05 02:19:23 +0530

    added regex for mobile

--- app/form.component.ts
+++ app/form.component.ts
@@ -15,24 +15,23 @@ onSubmit(){
 this.submitted=true;
     console.log("submitted");
 
-if(!this.myForm.valid){
-//alert("not valid")
-        }
-        else
-        {
-            //alert("is valid");
+                
             
          const user = new User(
 this.myForm.value.email,
 this.myForm.value.password,
 this.myForm.value.firstName,
-this.myForm.value.lastName
-
+this.myForm.value.lastName,
+this.myForm.value.mobile
         ) 
-      
+        if(this.myForm.valid)
+        {
+console.log(user);
 
         }
-    }
+      
+
+           }
 
 
 //nSubmit ends
@@ -42,16 +41,32 @@ ngOnInit(){
 
        
 
-        firstName : new FormControl(null,Validators.required,Validators.pattern("/^[a-z0-9]+$/i")),
-         lastName : new FormControl(null,Validators.required,Validators.pattern("/^[a-z0-9]+$/i")),
+        firstName : new FormControl(null,Validators.required),
+         lastName : new FormControl(null,Validators.required),
          email : new FormControl(null,[
 Validators.required,
 Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
 
          ]),
          password : new FormControl(null,Validators.required),
-  
-
-    })
+          mobile : new FormControl(null,[
+Validators.required ,  Validators.pattern("[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]")
+ ]),
+      })
+      
 }
+/*
+phoneno:string = "^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$"; 
+  myValidator(control: FormControl): {[s: string]: boolean} {
+    if (control.value.match(this.phoneno)) {
+      return {someval: true};
+    }
+    return null;
+  }
+
+ */
+
+
+
+
   }
