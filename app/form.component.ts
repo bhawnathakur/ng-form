cf9f80094506819ed134abc71db293d495f7c64f
bhawnathakur <bhawnathakur2@gmail.com>
2016-12-05 23:53:02 +0530

    Update form.component.ts
    conditonal error

--- app/form.component.ts
+++ app/form.component.ts
@@ -21,6 +21,38 @@ this.submitted=true;
 this.myForm.value.email,
 this.myForm.value.password,
 this.myForm.value.firstName,
+import { User } from './user.model';
+import { Component ,OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
+@Component({
+    
+  selector: 'my-form',
+  templateUrl: 'app/form.component.html',
+})
+export class FormComponent  implements OnInit {
+ constructor(private toastyService:ToastyService, private toastyConfig: ToastyConfig) { 
+        // Assign the selected theme name to the `theme` property of the instance of ToastyConfig. 
+        // Possible values: default, bootstrap, material
+        this.toastyConfig.theme = 'material';
+    }
+
+    
+
+
+public submitted : Boolean= false;
+myForm: FormGroup;
+
+onSubmit(){
+this.submitted=true;
+    console.log("submitted");
+
+                
+            
+         const user = new User(
+this.myForm.value.email,
+this.myForm.value.password,
+this.myForm.value.firstName,
 this.myForm.value.lastName,
 this.myForm.value.mobile,
 this.myForm.value.ssn
@@ -28,6 +60,11 @@ this.myForm.value.ssn
         if(this.myForm.valid)
         {
 console.log(user);
+
+        }
+        else
+        {
+this.addToast()
 
         }
       
@@ -70,6 +107,30 @@ phoneno:string = "^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
 
  */
 
+addToast() {
+        // Just add default Toast with title only
+        this.toastyService.default('Hi there');
+        // Or create the instance of ToastOptions
+        var toastOptions:ToastOptions = {
+            title:"not valid",
+            msg: "The message",
+            showClose: true,
+            timeout: 5000,
+            theme: 'default',
+            onAdd: (toast:ToastData) => {
+                console.log('Toast ' + toast.title + ' has been added!');
+            },
+            onRemove: function(toast:ToastData) {
+                console.log('Toast ' + toast.id + ' has been removed!');
+            }
+        };
+        // Add see all possible types in one shot
+        this.toastyService.info(toastOptions);
+       /* this.toastyService.success(toastOptions);
+        this.toastyService.wait(toastOptions);
+        this.toastyService.error(toastOptions);
+        this.toastyService.warning(toastOptions); */
+    }
 
 
 
