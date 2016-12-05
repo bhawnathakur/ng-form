a53d0f5348fe5be006bf44e544de8379418e418a
bhawnathakur <bhawnathakur2@gmail.com>
2016-12-05 23:51:51 +0530

    Update app.module.ts
    added toast module

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
-
+import {ToastyModule} from 'ng2-toasty';
 @NgModule({
-  imports:      [ BrowserModule,FormsModule, ReactiveFormsModule ],
+  imports:      [ BrowserModule,FormsModule, ReactiveFormsModule, ToastyModule.forRoot() ],
   declarations: [ AppComponent,FormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
